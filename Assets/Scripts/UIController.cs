ef3fa73f691b30c5914ecfb69eb8931641c65b22
cpasyanos <pasyanos.c@husky.neu.edu>
2017-09-19 17:30:09 -0400

    added trial limit

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -5,15 +5,49 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour {
 
+    public Canvas frontCanvas;
+
+    public Canvas leftCanvas;
+
+    public Canvas rightCanvas;
+
+    public Canvas ceilingCanvas;
+
+    public Canvas floorCanvas;
+
+    public GameObject targetCollider;
+
+    public Rigidbody Ball; // the ball 
+
+    public float speed; // the speed of the ball coming towards the target
+
+    public GameObject cameraRig; // the camerarig prefab
+
+    public float fieldWidth;
+
+    public float fieldDepth;
+
+    public float fieldHeight;
+
+    private bool isBall; // is there currently a ball?
+
+    private Rigidbody newBall; // the new ball that is instantiated 
+
+    private int targetDirection = 1;
+
+    private GameObject obj;
+
+    private float targetWidth = 6.5f;
+
+    private bool isGameOver = false;
+
     public int numTrials = 40;
 
-    private int trialsComplete = 0;
+    private int currTrial = 1;
 
-    [SerializeField]
-    private Text text;
+    public Text text;
 
-    [SerializeField]
-    private float timer = 5f;
+    public float timer = 5f;
 
     private float timeLeft;
 
@@ -23,23 +57,38 @@ public class UIController : MonoBehaviour {
 
     public static TimeUp OnReset;
 
+    public delegate void TrialsComplete();
+
+    public static TrialsComplete OnTrialsComplete;
+
     private AudioSource onTimeUp;
 
 	// Use this for initialization
 	void Start () {
-        TargetCollision.OnTargetHit += this.OnTargetHit;
+        TargetCollision.OnTargetHit += this.Reset;
         ControllerHandler.OnBallGrab += this.UpdateScore;
         OutOfBounds.OnOutOfBounds += this.OnFail;
 
         onTimeUp = GetComponent<AudioSource>();
 
+        // disable all targets except for front
+        frontCanvas.GetComponent<Image>().enabled = true;
+        leftCanvas.GetComponent<Image>().enabled = false;
+        rightCanvas.GetComponent<Image>().enabled = false;
+        ceilingCanvas.GetComponent<Image>().enabled = false;
+        floorCanvas.GetComponent<Image>().enabled = false;
+
         timeLeft = timer;
         score = 0;
-	}
+        MoveTarget();
+        CreateBall();
+
+        obj = new GameObject();
+    }
 
     void OnDisable()
     {
-        TargetCollision.OnTargetHit -= this.OnTargetHit;
+        TargetCollision.OnTargetHit -= this.Reset;
         ControllerHandler.OnBallGrab -= this.UpdateScore;
         OutOfBounds.OnOutOfBounds -= this.OnFail;
     }
@@ -47,19 +96,29 @@ public class UIController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        timeLeft -= Time.deltaTime;
-        text.text = "Time Left:" + Mathf.Round(timeLeft) + "\nScore: " + score;
 
-        if (timeLeft <= 0)
-        {
-            onTimeUp.Play();
+        if (currTrial < numTrials) {
+            timeLeft -= Time.deltaTime;
+            text.text = "Trial " + currTrial + " of " + numTrials +
+                "\nTime Left:" + Mathf.Round(timeLeft) + "\nScore: " + score;
 
-            if (OnReset != null)
+            if (timeLeft <= 0)
             {
-                OnReset();
-            }
+                onTimeUp.Play();
+                currTrial++;
 
-            timeLeft = timer;
+                if (OnReset != null)
+                {
+                    OnReset();
+                }
+
+                timeLeft = timer;
+
+            }
+        }
+        else
+        {
+            OnTrialsOver();
         }
     }
 
@@ -68,20 +127,198 @@ public class UIController : MonoBehaviour {
         score += 5;
     }
 
-    private void OnTargetHit()
-    {
-        UpdateScore();
+    private void Reset()
+    { 
 
-        if (OnReset != null)
+        if (currTrial < numTrials)
         {
-            OnReset();
-        }
+            UpdateScore();
 
-        timeLeft = timer;
+            if (OnReset != null)
+            {
+                OnReset();
+            }
+
+            timeLeft = timer;
+            currTrial++;
+        }
+        else
+        {
+            OnTrialsOver();
+        }
     }
 
     private void OnFail()
     {
-        timeLeft = timer;
+        // MoveTarget();
+        DestroyBall();
+        // CreateBall();
+
+        if (currTrial < numTrials)
+        {
+            MoveTarget();
+            CreateBall();
+            currTrial++;
+            timeLeft = timer;
+        }
+        else
+        {
+            OnTrialsOver();
+        }
+    }
+
+    private void OnTrialsOver()
+    {
+        text.text = "Congratulations!\nScore: " + score;
+
+        if (OnTrialsComplete != null)
+        {
+            OnTrialsComplete();
+        }
+    }
+
+    private void CreateBall()
+    {
+        if (!isGameOver)
+        {
+            isBall = true;
+            int direction = Random.Range(1, 4);
+
+            // set the gameobject that the ball will move towards
+            Vector3 posn = new Vector3(Random.Range(GameControl.Instance.leftMax, GameControl.Instance.rightMax),
+                Random.Range(0, GameControl.Instance.heightMax), cameraRig.transform.position.z);
+            obj.transform.position = posn;
+
+            // target and ball should not come from same direction 
+            while (direction == targetDirection)
+            {
+                direction = Random.Range(1, 4);
+            }
+
+            float x, y, z;
+
+            y = Random.Range(cameraRig.transform.position.y, fieldHeight);
+
+            switch (direction)
+            {
+                // comes from front
+                case 1:
+                    x = Random.Range(cameraRig.transform.position.x - fieldWidth / 2f, cameraRig.transform.position.x + fieldWidth / 2f);
+                    z = cameraRig.transform.position.z + fieldDepth;
+                    break;
+
+                // comes from left
+                case 2:
+                    x = cameraRig.transform.position.x - fieldWidth / 2;
+                    z = Random.Range(cameraRig.transform.position.z, cameraRig.transform.position.z + fieldDepth);
+                    break;
+
+                // comes from right
+                default:
+                    x = cameraRig.transform.position.x + fieldWidth / 2;
+                    z = Random.Range(cameraRig.transform.position.z, cameraRig.transform.position.z + fieldDepth);
+                    break;
+            }
+
+            newBall = Instantiate(Ball, new Vector3(x, y, z), Ball.transform.rotation);
+            newBall.transform.LookAt(obj.transform);
+            newBall.AddRelativeForce(Vector3.forward * speed, ForceMode.Acceleration);
+        }
+    }
+
+    private void MoveTarget()
+    {
+        int direction = Random.Range(1, 6);
+
+        BoxCollider col = targetCollider.GetComponent<BoxCollider>();
+
+        // disable whichever target was previously enabled
+        switch (targetDirection)
+        {
+            case 1:
+                frontCanvas.GetComponent<Image>().enabled = false;
+                break;
+            case 2:
+                leftCanvas.GetComponent<Image>().enabled = false;
+                break;
+            case 3:
+                rightCanvas.GetComponent<Image>().enabled = false;
+                break;
+            case 4:
+                floorCanvas.GetComponent<Image>().enabled = false;
+                break;
+            default:
+                ceilingCanvas.GetComponent<Image>().enabled = false;
+                break;
+        }
+
+        if (!isGameOver)
+        {
+
+            // now rotate and move to appropriate spot along with collider
+            switch (direction)
+            {
+                // front
+                case 1:
+                    frontCanvas.GetComponent<Image>().enabled = true;
+                    frontCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
+                        = new Vector3(Random.Range(-fieldWidth / 2, fieldWidth / 2),
+                        frontCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
+                        frontCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.z);
+                    col.size = new Vector3(targetWidth, targetWidth, 0.3f);
+                    targetCollider.transform.position = frontCanvas.transform.position;
+                    break;
+                // left side
+                case 2:
+                    leftCanvas.GetComponent<Image>().enabled = true;
+                    leftCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
+                        = new Vector3(leftCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.x,
+                        leftCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
+                        Random.Range(0, fieldDepth));
+                    col.size = new Vector3(0.3f, targetWidth, targetWidth);
+                    targetCollider.transform.position = leftCanvas.transform.position;
+                    break;
+                // right side
+                case 3:
+                    rightCanvas.GetComponent<Image>().enabled = true;
+                    rightCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
+                        = new Vector3(rightCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.x,
+                        rightCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
+                        Random.Range(0, fieldDepth));
+                    col.size = new Vector3(0.3f, targetWidth, targetWidth);
+                    targetCollider.transform.position = rightCanvas.transform.position;
+                    break;
+                // bottom
+                case 4:
+                    floorCanvas.GetComponent<Image>().enabled = true;
+                    floorCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
+                        = new Vector3(Random.Range(-fieldWidth / 2, fieldWidth / 2),
+                        floorCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
+                        floorCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.z);
+                    col.size = new Vector3(targetWidth, 0.3f, targetWidth);
+                    targetCollider.transform.position = floorCanvas.transform.position;
+                    break;
+                // top
+                default:
+                    ceilingCanvas.GetComponent<Image>().enabled = true;
+                    ceilingCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
+                        = new Vector3(Random.Range(-fieldWidth / 2, fieldWidth / 2),
+                        ceilingCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
+                        ceilingCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.z);
+                    col.size = new Vector3(targetWidth, 0.3f, targetWidth);
+                    targetCollider.transform.position = ceilingCanvas.transform.position;
+                    break;
+            }
+
+            targetDirection = direction;
+
+        }
+
+    }
+
+    private void DestroyBall()
+    {
+        Destroy(newBall.gameObject);
+        isBall = false;
     }
 }
--- Assets/Scripts/makeBalls.cs
+++ Assets/Scripts/makeBalls.cs
@@ -38,10 +38,13 @@ public class makeBalls : MonoBehaviour {
 
     private float targetWidth = 6.5f;
 
+    private bool isGameOver = false;
+
     // initialization of another ball
     void Start () {
         UIController.OnReset += Reset;
         OutOfBounds.OnOutOfBounds += Reset;
+        UIController.OnTrialsComplete += GameOver;
 
         obj = new GameObject();
 
@@ -60,53 +63,55 @@ public class makeBalls : MonoBehaviour {
     {
         UIController.OnReset -= Reset;
         OutOfBounds.OnOutOfBounds -= Reset;
+        UIController.OnTrialsComplete -= GameOver;
     }
 
     private void CreateBall()
     {
-        isBall = true;
-        int direction = Random.Range(1, 4);
+        if (!isGameOver) {
+            isBall = true;
+            int direction = Random.Range(1, 4);
 
-        // set the gameobject that the ball will move towards
-        Vector3 posn = new Vector3(Random.Range(GameControl.Instance.leftMax, GameControl.Instance.rightMax),
-            Random.Range(0, GameControl.Instance.heightMax), cameraRig.transform.position.z);
-        obj.transform.position = posn;
-        Debug.Log(posn);
+            // set the gameobject that the ball will move towards
+            Vector3 posn = new Vector3(Random.Range(GameControl.Instance.leftMax, GameControl.Instance.rightMax),
+                Random.Range(0, GameControl.Instance.heightMax), cameraRig.transform.position.z);
+            obj.transform.position = posn;
 
-        // target and ball should not come from same direction 
-        while (direction == targetDirection)
-        {
-            direction = Random.Range(1, 4);
+            // target and ball should not come from same direction 
+            while (direction == targetDirection)
+            {
+                direction = Random.Range(1, 4);
+            }
+
+            float x, y, z;
+
+            y = Random.Range(cameraRig.transform.position.y, fieldHeight);
+
+            switch (direction)
+            {
+                // comes from front
+                case 1:
+                    x = Random.Range(cameraRig.transform.position.x - fieldWidth / 2f, cameraRig.transform.position.x + fieldWidth / 2f);
+                    z = cameraRig.transform.position.z + fieldDepth;
+                    break;
+
+                // comes from left
+                case 2:
+                    x = cameraRig.transform.position.x - fieldWidth / 2;
+                    z = Random.Range(cameraRig.transform.position.z, cameraRig.transform.position.z + fieldDepth);
+                    break;
+
+                // comes from right
+                default:
+                    x = cameraRig.transform.position.x + fieldWidth / 2;
+                    z = Random.Range(cameraRig.transform.position.z, cameraRig.transform.position.z + fieldDepth);
+                    break;
+            }
+
+            newBall = Instantiate(Ball, new Vector3(x, y, z), Ball.transform.rotation);
+            newBall.transform.LookAt(obj.transform);
+            newBall.AddRelativeForce(Vector3.forward * speed, ForceMode.Acceleration);
         }
-
-        float x, y, z;
-
-        y = Random.Range(cameraRig.transform.position.y, fieldHeight);
-
-        switch (direction)
-        {
-            // comes from front
-            case 1:
-                x = Random.Range(cameraRig.transform.position.x - fieldWidth / 2f, cameraRig.transform.position.x + fieldWidth / 2f);
-                z = cameraRig.transform.position.z + fieldDepth;
-                break;
-            
-            // comes from left
-            case 2:
-                x = cameraRig.transform.position.x - fieldWidth / 2;
-                z = Random.Range(cameraRig.transform.position.z, cameraRig.transform.position.z + fieldDepth);
-                break;
-
-            // comes from right
-            default:
-                x = cameraRig.transform.position.x + fieldWidth / 2;
-                z = Random.Range(cameraRig.transform.position.z, cameraRig.transform.position.z + fieldDepth);
-                break;
-        }
-
-        newBall = Instantiate(Ball, new Vector3(x, y, z), Ball.transform.rotation);
-        newBall.transform.LookAt(obj.transform);
-        newBall.AddRelativeForce(Vector3.forward*speed, ForceMode.Acceleration);
     }
 
     private void MoveTarget()
@@ -135,62 +140,66 @@ public class makeBalls : MonoBehaviour {
                 break;
         }
 
-        // now rotate and move to appropriate spot along with collider
-        switch (direction)
-        {
-            // front
-            case 1:
-                frontCanvas.GetComponent<Image>().enabled = true;
-                frontCanvas.GetComponent<Image>().GetComponent<RectTransform>().position 
-                    = new Vector3(Random.Range(- fieldWidth / 2, fieldWidth / 2), 
-                    frontCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
-                    frontCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.z);
-                col.size = new Vector3(targetWidth, targetWidth, 0.3f);
-                targetCollider.transform.position = frontCanvas.transform.position;
-                break;
-            // left side
-            case 2:
-                leftCanvas.GetComponent<Image>().enabled = true;
-                leftCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
-                    = new Vector3(leftCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.x,
-                    leftCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
-                    Random.Range(0, fieldDepth));
-                col.size = new Vector3(0.3f, targetWidth, targetWidth);
-                targetCollider.transform.position = leftCanvas.transform.position;
-                break;
-            // right side
-            case 3:
-                rightCanvas.GetComponent<Image>().enabled = true;
-                rightCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
-                    = new Vector3(rightCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.x,
-                    rightCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
-                    Random.Range(0, fieldDepth));
-                col.size = new Vector3(0.3f, targetWidth, targetWidth);
-                targetCollider.transform.position = rightCanvas.transform.position;
-                break;
-            // bottom
-            case 4:
-                floorCanvas.GetComponent<Image>().enabled = true;
-                floorCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
-                    = new Vector3(Random.Range(-fieldWidth / 2, fieldWidth / 2),
-                    floorCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
-                    floorCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.z);
-                col.size = new Vector3(targetWidth, 0.3f, targetWidth);
-                targetCollider.transform.position = floorCanvas.transform.position;
-                break;
-            // top
-            default:
-                ceilingCanvas.GetComponent<Image>().enabled = true;
-                ceilingCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
-                    = new Vector3(Random.Range(-fieldWidth / 2, fieldWidth / 2),
-                    ceilingCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
-                    ceilingCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.z);
-                col.size = new Vector3(targetWidth, 0.3f, targetWidth);
-                targetCollider.transform.position = ceilingCanvas.transform.position;
-                break;
-        }
+        if (!isGameOver) {
 
-        targetDirection = direction;
+            // now rotate and move to appropriate spot along with collider
+            switch (direction)
+            {
+                // front
+                case 1:
+                    frontCanvas.GetComponent<Image>().enabled = true;
+                    frontCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
+                        = new Vector3(Random.Range(-fieldWidth / 2, fieldWidth / 2),
+                        frontCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
+                        frontCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.z);
+                    col.size = new Vector3(targetWidth, targetWidth, 0.3f);
+                    targetCollider.transform.position = frontCanvas.transform.position;
+                    break;
+                // left side
+                case 2:
+                    leftCanvas.GetComponent<Image>().enabled = true;
+                    leftCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
+                        = new Vector3(leftCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.x,
+                        leftCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
+                        Random.Range(0, fieldDepth));
+                    col.size = new Vector3(0.3f, targetWidth, targetWidth);
+                    targetCollider.transform.position = leftCanvas.transform.position;
+                    break;
+                // right side
+                case 3:
+                    rightCanvas.GetComponent<Image>().enabled = true;
+                    rightCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
+                        = new Vector3(rightCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.x,
+                        rightCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
+                        Random.Range(0, fieldDepth));
+                    col.size = new Vector3(0.3f, targetWidth, targetWidth);
+                    targetCollider.transform.position = rightCanvas.transform.position;
+                    break;
+                // bottom
+                case 4:
+                    floorCanvas.GetComponent<Image>().enabled = true;
+                    floorCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
+                        = new Vector3(Random.Range(-fieldWidth / 2, fieldWidth / 2),
+                        floorCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
+                        floorCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.z);
+                    col.size = new Vector3(targetWidth, 0.3f, targetWidth);
+                    targetCollider.transform.position = floorCanvas.transform.position;
+                    break;
+                // top
+                default:
+                    ceilingCanvas.GetComponent<Image>().enabled = true;
+                    ceilingCanvas.GetComponent<Image>().GetComponent<RectTransform>().position
+                        = new Vector3(Random.Range(-fieldWidth / 2, fieldWidth / 2),
+                        ceilingCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.y,
+                        ceilingCanvas.GetComponent<Image>().GetComponent<RectTransform>().position.z);
+                    col.size = new Vector3(targetWidth, 0.3f, targetWidth);
+                    targetCollider.transform.position = ceilingCanvas.transform.position;
+                    break;
+            }
+
+            targetDirection = direction;
+
+        }
 
     }
 
@@ -202,8 +211,35 @@ public class makeBalls : MonoBehaviour {
 
     private void Reset()
     {
-        MoveTarget();
-        DestroyBall();
-        CreateBall();
+        if (!isGameOver) {
+            MoveTarget();
+            DestroyBall();
+            CreateBall();
+        }
+    }
+
+    private void GameOver()
+    {
+        isGameOver = true;
+
+        // disable whichever target was previously enabled
+        switch (targetDirection)
+        {
+            case 1:
+                frontCanvas.GetComponent<Image>().enabled = false;
+                break;
+            case 2:
+                leftCanvas.GetComponent<Image>().enabled = false;
+                break;
+            case 3:
+                rightCanvas.GetComponent<Image>().enabled = false;
+                break;
+            case 4:
+                floorCanvas.GetComponent<Image>().enabled = false;
+                break;
+            default:
+                ceilingCanvas.GetComponent<Image>().enabled = false;
+                break;
+        } 
     }
 }
