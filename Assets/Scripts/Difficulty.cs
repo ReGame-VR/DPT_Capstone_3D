213e555cca026b9241309a8a27fc967625537b03
cpasyanos <pasyanos.c@husky.neu.edu>
2017-10-03 14:46:28 -0400

    made some tweaks to Difficulty class

--- Assets/Scripts/Difficulty.cs
+++ Assets/Scripts/Difficulty.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 
 public static class Difficulty {
-
-    public static readonly float[] sunScale = new float[3] { 1f, 0.75f, 0.5f };
-    public static readonly int[] scoreDecay = new int[3] { 0, 0, 0 }; // TODO: TRANSLATE IN TERMS OF FRAMERATE
-    public static readonly float[] delay = new float[3] { 0, 0, 0 };  // THIS ONE TOO
-
+    public static readonly float[] sunScale = new float[4] { 1f, 0.75f, 0.5f, 0.25f };
+    public static readonly int[] scoreDecay = new int[4] { 0, 30, 15, 7};
+    public static readonly float[] delay = new float[4] { 3f, 1.5f, 0.5f, 0.25f };  
 }
